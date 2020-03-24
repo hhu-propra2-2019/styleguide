6ccd06f4c72784c99d15bfc4aa738c5e02a3dd86
Wilhelm Alexander Maximilian Thelen <git@alexanderthelen.com>
2020-03-24 10:17:03 +0100

    Move account creation to controller advice

--- src/main/java/mops/styleguide/presentation/web/StyleGuideController.java
+++ src/main/java/mops/styleguide/presentation/web/StyleGuideController.java
@@ -2,15 +2,12 @@ package mops.styleguide.presentation.web;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import mops.styleguide.infrastructure.Account;
 import mops.styleguide.infrastructure.AccountService;
-import mops.styleguide.infrastructure.KeycloakAccountService;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -26,11 +23,6 @@ public class StyleGuideController {
         authenticatedAccess = registry.counter("access.authenticated");
         publicAccess = registry.counter("access.public");
         this.accountService = accountService;
-    }
-
-    @ModelAttribute("account")
-    private Account getAccount(KeycloakAuthenticationToken token, KeycloakAccountService accountService) {
-        return token != null ? accountService.createAccountFromToken(token) : null;
     }
 
     @GetMapping("/")
